e7f1e11d67085a32d8ecd6e4edb75c5a37fe8bfc
Mallarie <mallariemilano@gmail.com>
2019-02-26 15:15:54 -0800

    Second test for keywords and horns passed

--- test/tests.js
+++ test/tests.js
@@ -1,29 +1,39 @@
 import './html-equal.js';
 
 const test = QUnit.test;
-//this is to add a title on the test page, so we know what test it refers to
+
 QUnit.module('image filters');
 
 function filteredImages(images, filter) {
     return images.filter(image => {
-        const keyword = image.keyword === filter.keyword;
-        return keyword;
+        const keyword = !filter.keyword || image.keyword === filter.keyword;
+        const horns = !filter.horns || image.horns >= filter.horns;
+        return keyword && horns;
     });
 }
 
-test('filter images by keyword', assert => {
-    //arrgh
-    const images = [
-        { title: 'a thing', keyword: 'banana', horns: 2 },
-        { title: 'another thing', keyword: 'banana', horns: 5 },
-        { title: 'a third thing', keyword: 'pineapple', horns: 1 }
-    ];
+const images = [
+    { title: 'a thing', keyword: 'banana', horns: 2 },
+    { title: 'another thing', keyword: 'banana', horns: 5 },
+    { title: 'a third thing', keyword: 'pineapple', horns: 1 }
+];
 
+test('filter images by keyword', assert => {
     const filteredObject = { keyword: 'banana' };
-    //act
+    
     const filtered = filteredImages(images, filteredObject);
 
-//assert
+    assert.deepEqual(filtered, [
+        { title: 'a thing', keyword: 'banana', horns: 2 },
+        { title: 'another thing', keyword: 'banana', horns: 5 }
+    ]);
+});
+
+test('filter images by number of horns and keyword', assert => {
+    const filterHorns = { horns: 2 };
+    
+    const filtered = filteredImages(images, filterHorns);
+    
     assert.deepEqual(filtered, [
         { title: 'a thing', keyword: 'banana', horns: 2 },
         { title: 'another thing', keyword: 'banana', horns: 5 }
